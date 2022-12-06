9aab34c6e183d694de4a62ce4ec443eb234a638f
Jude <87612608+jude52@users.noreply.github.com>
2022-12-06 22:59:58 +0000

    Added delete functionality

--- NoteTakingApp/NoteTakingApp/Models/NoteTaking.cs
+++ NoteTakingApp/NoteTakingApp/Models/NoteTaking.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using Microsoft.AspNetCore.Components;
+
 namespace NoteTakingApp.Models
 {
     public class NoteTaking
     {
-       
-    
+        [Parameter]
         public int Id { get; set; }
         public string Title { get; set; } = String.Empty;
         public string? Message { get; set; }
--- NoteTakingApp/NoteTakingApp/Models/PersonModel.cs
+++ NoteTakingApp/NoteTakingApp/Models/PersonModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using Microsoft.AspNetCore.Components;
+
 namespace NoteTakingApp.Models
 {
     public class PersonModel
     {
-       
+        [Parameter]
             public int Id { get; set; }
             public string FirstName { get; set; } = String.Empty;
             public string? LastName { get; set; }
